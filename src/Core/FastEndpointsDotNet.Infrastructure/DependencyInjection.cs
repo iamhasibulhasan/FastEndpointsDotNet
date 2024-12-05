9c2be103c20c81fad79f2c9e8629f05068d3df7f
iamhasibulhasan <hashibul218@gmail.com>
2024-12-05 11:40:31 +0600

    Infrastructure setup

---
+++ src/Core/FastEndpointsDotNet.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpointsDotNet.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.AutoRegisterDi;
+using System.Reflection;
+
+namespace FastEndpointsDotNet.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<FEDbContext>(options =>
+        options.UseNpgsql(configuration.GetConnectionString("DbConnection"), b => b.MigrationsAssembly(typeof(FEDbContext).Assembly.FullName)));
+
+        Dependency(services);
+        return services;
+    }
+    public static void Dependency(this IServiceCollection services)
+    {
+        var assembliesToScan = new[]
+        {
+            Assembly.GetExecutingAssembly(),
+            Assembly.GetAssembly(typeof(StudentService)),
+            Assembly.GetAssembly(typeof(StudentRepository))
+        };
+        services.RegisterAssemblyPublicNonGenericClasses(assembliesToScan)
+            .Where(c => c.Name.EndsWith("Service") || c.Name.EndsWith("Repository"))
+            .AsPublicImplementedInterfaces();
+    }
+}
+
---
+++ src/Core/FastEndpointsDotNet.Infrastructure/Persistence/Configurations/Entities/Employees/EmployeeConfig.cs
@@ -0,0 +1,27 @@
+﻿using FastEndpointsDotNet.Domain.Entities.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FastEndpointsDotNet.Infrastructure.Persistence.Configurations.Entities.Employees;
+
+public sealed class EmployeeConfig : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.ToTable("Employees", "public");
+        builder.HasKey(k => k.Id);
+
+        builder.Property(k => k.EmployeeCode)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(k => k.FirstName)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(k => k.LastName)
+            .HasMaxLength(200);
+        builder.Property(k => k.Email)
+            .IsRequired();
+        builder.Property(k => k.Phone)
+            .IsRequired();
+    }
+}
